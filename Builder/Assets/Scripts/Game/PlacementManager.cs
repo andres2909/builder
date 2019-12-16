ad29da7138a849a8873df216a647a000732138d5
Andres Mijares <andresalirio_07@hotmail.com>
2019-12-16 18:28:06 -0500

    Save changes

--- Builder/Assets/Scripts/Game/PlacementManager.cs
+++ Builder/Assets/Scripts/Game/PlacementManager.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class PlacementManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _placeableObjectPrefab;
+    public GameObject _placeableObjectPrefab;
 
     private GameObject currentPlaceableObject;
     private float mouseWheelRotation;
